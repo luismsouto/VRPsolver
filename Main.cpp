bef5e3b9b953485c013b66472b89149b78eaec48
Luís Souto <luisfmsouto@gmail.com>
2018-03-18 17:01:44 +0000

    First commit with gitignore

---
+++ Main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+vector <vector <double>> distances;
+bool importData();
+bool importDistances();
+
+
+int main() {
+	importData();
+	for (int i = 0; i < distances.size(); i++) {
+		for (int j = 0; j < distances[i].size(); j++) {
+			cout << distances[i][j] << " " ;
+		}
+		cout << endl;
+	}
+	system("pause");
+	return 0;
+}
+
+
+bool importData() {
+	string line;
+	double val;
+	int nLines = 0, k = 1;
+	vector <double> lineVector;
+	ifstream fileInput;
+	fileInput.open("Distances.csv");
+	while (fileInput.good()) {
+		while (getline(fileInput, line)) {
+			nLines++;
+		}
+	}
+	fileInput.close();
+	fileInput.open("Distances.csv");
+	while (fileInput.good()) {
+		while (getline(fileInput, line, ',')) {
+			istringstream stream(line);
+			while (stream >> val) {
+				lineVector.push_back(val);
+				if (k < nLines) {
+					++k;
+				}
+				else {
+					distances.push_back(lineVector);
+					lineVector.clear();
+					k = 1;
+				}
+			}
+		}
+	}
+	return true;
+}
